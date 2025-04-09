3558c01bc6f37d8da58dfae0326b58d948fb13d4
Donovan Hutchence <slimbuck7@gmail.com>
2025-04-09 14:45:48 +0100

    latest

--- src/ui/timeline-panel.ts
+++ src/ui/timeline-panel.ts
@@ -66,7 +66,7 @@ class Ticks extends Container {
                 keys.push(label);
             };
 
-            (events.invoke('timeline.keys') as number[])?.forEach(createKey);
+            (events.invoke('timeline.keys') as number[]).forEach(createKey);
 
             addKey = (value: number) => {
                 createKey(value);
